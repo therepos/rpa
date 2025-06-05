6786a90b1e7ee52e2244bbb35d863d07f4fa6de4
The Repos <ry4nyeo@gmail.com>
2025-06-05 14:06:27 +0800

    updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,7 +60,7 @@ export default {
           position: 'right',
         },
         {
-          href: 'https://github.com/${org}/${repo}',
+          href: `https://github.com/${org}/${repo}1,
           position: 'right',
           className: 'header-github-link',
           'aria-label': 'GitHub repository',
