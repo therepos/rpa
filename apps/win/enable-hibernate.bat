a139b69eb3bfe9a8c8f98752559831fe8168d7de
The Repos <ry4nyeo@gmail.com>
2026-01-01 18:52:06 +0800

    up

--- apps/win/enable-hibernate.bat
+++
@@ -1,3 +0,0 @@
-powercfg /hibernate on
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FlyoutMenuSettings" /v ShowHibernateOption /t REG_DWORD /d 1 /f
-taskkill /f /im explorer.exe & start explorer.exe
